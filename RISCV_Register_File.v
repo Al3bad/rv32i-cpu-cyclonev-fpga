41e6c5772a9b27dae98341eac35ca33249c374c9
Abdullah Alabbad <a.a.al3bad@gmail.com>
2021-05-25 22:22:50 +1000

    fix register file implementation

--- RISCV_Register_File.v
+++ RISCV_Register_File.v
@@ -1,4 +1,5 @@
 
+
 module RISCV_Register_File (
     input clk, regWrite,
 
@@ -12,24 +13,20 @@ module RISCV_Register_File (
 //  <reg-width> <array-name> <array-size>
 reg [31:0] registers [31:0];
 
-// hardwire x0 to 0
-//assign registers[0] = 32'd0;
-
-// [TEMPORARY] Initialise the registers 
-// initial begin
-//     registers[1] = 32'd4;
-//     registers[2] = 32'd6;
-// end
 
 always @(posedge clk) begin
-    if (regWrite) begin
-        // write the new 'data' to 'rd'
-        registers[rd] <= data;
-    end else begin
-        output1 <= rs1? registers[rs1] : 32'd0;
-        output2 <= rs2? registers[rs2]: 32'd0;
-    end
-    
+    // Write the new 'data' to 'rd'
+    if (regWrite && (rd != 5'h00)) registers[rd] <= data;
+
+    // // Output the content of the selected registers
+    // output1 <= rs1? registers[rs1] : 32'h00;
+    // output2 <= rs2? registers[rs2] : 32'h00;
+end
+
+always @(rs1, rs2) begin
+    // Output the content of the selected registers
+    output1 <= rs1? registers[rs1] : 32'h00;
+    output2 <= rs2? registers[rs2] : 32'h00;
 end
 
     
--- test_Register_File.v
+++ test_Register_File.v
@@ -3,13 +3,17 @@
 `timescale 1ns/1ps
 
 module test_RegisterFile;
-reg clk;
-reg [4:0] rs1, rs2;
+reg clk, regWrite;
+reg [4:0] rd, rs1, rs2;
+reg [31:0] data;
 wire [31:0] output1, output2;
 
-RISCV_RegisterFile regFile (.clk(clk),
+RISCV_Register_File regFile (.clk(clk),
+                            .regWrite(regWrite),
+                            .data(data),
                             .rs1(rs1), 
-                            .rs2(rs2), 
+                            .rs2(rs2),
+                            .rd(rd),
                             .output1(output1), 
                             .output2(output2));
 
@@ -23,12 +27,37 @@ end
 
 always @(posedge clk) begin
     rs1 = 5'd0;
+    rs2 = 5'd0;
+    rd = 5'd0;
+    regWrite = 1'b0;
+    data = 32'd0;
+    #2
+
+    regWrite = 1'b1;
+    rd  = 5'd1;
+    data = 32'd10;
+    #2;
+
+    rd  = 5'd2;
+    #2;
+
+    regWrite = 1'b0;
+    rs1 = 5'd0;
     rs2 = 5'd1;
-    #4;
+    #2;
 
     rs1 = 5'd1;
     rs2 = 5'd2;
-    #4;
+    #2;
+
+    regWrite = 1'b1;
+    data = 32'd15;
+    rd = 5'd1;
+    rs1 = 5'd1;
+    #2;
+
+    regWrite = 1'b0;
+    #2;
 
     $display("All Done!");
     $stop;
