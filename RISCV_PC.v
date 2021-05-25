a9cbe62cd3e2eef731deeddf97bda38f2da66189
Abdullah Alabbad <a.a.al3bad@gmail.com>
2021-05-25 22:28:52 +1000

    fix program counter implementation to use offset instead of actual addresses

--- RISCV_PC.v
+++ RISCV_PC.v
@@ -3,29 +3,22 @@
 * pcEn: enable signal - update the counter if 1
 * pcSrc: signal that choses between PC + 0x4 or the address input (for branching)
 *
-* addressInput: instruction address that should loaded to the program counter to jump/brach to
+* offset: the offset of the destination instruction address from the current one
 */
 
 module RISCV_PC (
     input clk, pcSrc, pcEn,
-    input [31:0] addressInput,
+    input [31:0] offset,
 
     output reg [7:0] pcOutput
 );
 
-// the value that should be used to inrement the PC
+// the value that should be used to increment the PC
 localparam INC_BY = 32'h1;
-
-reg pc;
-
-// Initialse the program counter start from address 0
-// initial begin
-//     pcOutput = 31'h0;
-// end
 
 always @(posedge clk ) begin
     if (pcEn) begin
-        pcOutput <= pcSrc? addressInput : (pcOutput + INC_BY);
+        pcOutput <= pcSrc? (pcOutput + $signed(offset)) : (pcOutput + INC_BY);
     end
 end
 
