3e511effd447f42c68a2700cd50c68ccfa19511c
Abdullah Alabbad <a.a.al3bad@gmail.com>
2021-06-08 16:54:59 +1000

    delete the comment for the old implementation of regsiter file module

--- RISCV_Register_File.v
+++ RISCV_Register_File.v
@@ -17,10 +17,6 @@ reg [31:0] registers [31:0];
 always @(posedge clk) begin
     // Write the new 'data' to 'rd'
     if (regWrite && (rd != 5'h00)) registers[rd] <= data;
-
-    // // Output the content of the selected registers
-    // output1 <= rs1? registers[rs1] : 32'h00;
-    // output2 <= rs2? registers[rs2] : 32'h00;
 end
 
 always @(rs1, rs2) begin
