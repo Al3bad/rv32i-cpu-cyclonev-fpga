4e7b8ea42346e57b6b051360e6dcc900442ebdfe
Abdullah Alabbad <a.a.al3bad@gmail.com>
2021-07-28 18:52:23 +1000

    reorganise and cleanup the block diagram

--- modelsim_src/RISCV_Forwarding_Unit.v
+++ modelsim_src/RISCV_Forwarding_Unit.v
@@ -5,11 +5,11 @@ module RISCV_Forwarding_Unit (
 );
 
 always @(WB_RegWrite, WB_rd, rs1, rs2) begin
-    if (WB_RegWrite == 1'b1 && WB_rd != 5'h00 && WB_rd == rs1) forwardA = 2'b00;  // Forward ALU result to A
-    else forwardA = 2'b10;
+    if (WB_RegWrite == 1'b1 && WB_rd != 5'h00 && WB_rd == rs1) forwardA = 2'b10;  // Forward ALU result to A
+    else forwardA = 2'b00;
 
-    if (WB_RegWrite == 1'b1 && WB_rd != 5'h00 && WB_rd == rs2) forwardB = 2'b00;  // Forward ALU result to B
-    else forwardB = 2'b10;
+    if (WB_RegWrite == 1'b1 && WB_rd != 5'h00 && WB_rd == rs2) forwardB = 2'b10;  // Forward ALU result to B
+    else forwardB = 2'b00;
 end
     
 endmodule
